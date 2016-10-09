84e785481bea154b2e4e5abfb47626dc9ef74955
Евгений <Dozer95@bk.ru>
2016-10-09 15:53:14 +0500

    Добавил фрагмент "Список рецептов"

---
+++ app/src/main/java/com/cookbook/fragments/RecipesListFragment.java
@@ -0,0 +1,96 @@
+package com.cookbook.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.cookbook.MainActivity;
+import com.cookbook.R;
+import com.cookbook.adapters.RecipeListAdapter;
+import com.cookbook.dummy.DummyRecipes;
+import com.cookbook.pojo.Recipe;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+
+public class RecipesListFragment extends Fragment implements Recipe.RecipeClickListener {
+    private static final String ARG_CAT_NAME = "catName";
+    private static final String ARG_RECIPES = "recipes";
+    private static final Gson gson = new Gson();
+    private static final Type type = new TypeToken<List<Recipe>>(){}.getType();
+
+    List<Recipe> recipes;
+    RecyclerView recyclerView;
+
+    public RecipesListFragment() {
+        // Required empty public constructor
+    }
+
+    public static RecipesListFragment newInstance(List<Recipe> recipes, String categoryName) {
+        RecipesListFragment fragment = new RecipesListFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_RECIPES, gson.toJson(recipes, type));
+        args.putString(ARG_CAT_NAME,categoryName);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            recipes = gson.fromJson(getArguments().getString(ARG_RECIPES),type);
+            String name = getArguments().getString(ARG_CAT_NAME);
+            getActivity().setTitle(name);
+        }
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_recipes_list, container, false);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        menu.clear();
+        inflater.inflate(R.menu.search_recipe_menu, menu);
+        super.onCreateOptionsMenu(menu,inflater);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        recipes = DummyRecipes.getRecipes(getContext(),20);
+        initRecycleView();
+    }
+
+    private void initRecycleView() {
+        recyclerView = (RecyclerView)getView().findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+
+        RecipeListAdapter adapter = new RecipeListAdapter(getContext(),recipes,this);
+        recyclerView.setAdapter(adapter);
+    }
+
+
+    @Override
+    public void onClick(Recipe recipe) {
+        RecipeFragment fragment = new RecipeFragment();
+        MainActivity mainActivity = (MainActivity) getActivity();
+        mainActivity.setFragment(fragment,true);
+        //Snackbar.make(getActivity().findViewById(R.id.root_layout),String.format("Click on %s",recipe.name),Snackbar.LENGTH_SHORT).show();
+    }
+
+
+}
