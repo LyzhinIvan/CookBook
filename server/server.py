0925bc391fba114194f60bd43dd823e9f11907ca
ms-lilibeth <ms.lilibeth@yandex.ru>
2016-11-15 22:54:27 +0500

    Returning recipe-ingredients connections fixed

--- server/server.py
+++ server/server.py
@@ -11,10 +11,18 @@ import sys
 import os
 from datetime import datetime
 
+# ======================================== Aliases
+RECIPES_FIELD_NAME = 'recipes'
+CATEGORIES_FIELD_NAME = 'categories'
+INGREDIENTS_FIELD_NAME = 'ingredients'
+RECIPE_INGREDIENTS_FIELD_NAME = 'recipeIngredients'
+NEW_UPDATED_FIELD_NAME = 'newUpdated'
+# ========================================
 
 def get_new_records(last_updated):
-    result = {NEW_UPDATED_FIELD_NAME: int(datetime.timestamp(datetime.now()))}
-
+    result = {CATEGORIES_FIELD_NAME: [], INGREDIENTS_FIELD_NAME: [], RECIPES_FIELD_NAME: [],
+              RECIPE_INGREDIENTS_FIELD_NAME: [],
+              NEW_UPDATED_FIELD_NAME: int(datetime.timestamp(datetime.now()))}
     try:
 
         """ Adding recipes """
@@ -33,8 +41,29 @@ def get_new_records(last_updated):
             tmp_rec['category'] = s.category.id
             recipes.append(tmp_rec)
         result[RECIPES_FIELD_NAME] = recipes
-        del recipes
 
+        recipes = [r['id'] for r in recipes]
+        """ Adding recipe-ingredients connections """
+        select_result = models.RecipeIngredient.select()
+        select_result = models.RecipeIngredient.select().where(models.RecipeIngredient.recipe_id in recipes)
+        rec_ing = []
+
+        for s in select_result:
+            if s.id is None:
+                continue
+            tmp_rec_ing = {}
+            tmp_rec_ing['id'] = s.id
+            tmp_rec_ing['resId'] = s.recipe_id
+            tmp_rec_ing['ingId'] = s.ingredient_id
+            tmp_rec_ing['quantity'] = s.quantity
+            rec_ing.append(tmp_rec_ing)
+        result[RECIPE_INGREDIENTS_FIELD_NAME] = rec_ing
+        del rec_ing
+
+    except peewee.DoesNotExist:
+        pass
+
+    try:
         """ Adding categories """
         select_result = models.Category.select().where(models.Category.timestamp_added > last_updated)
         categories = []
@@ -48,7 +77,10 @@ def get_new_records(last_updated):
             categories.append(tmp_cat)
         result[CATEGORIES_FIELD_NAME] = categories
         del categories
+    except peewee.DoesNotExist:
+        pass
 
+    try:
         """Adding ingredients """
         select_result = models.Ingredient.select().where(models.Ingredient.timestamp_added > last_updated)
         ingredients = []
@@ -62,11 +94,8 @@ def get_new_records(last_updated):
             ingredients.append(tmp_ing)
         result[INGREDIENTS_FIELD_NAME] = ingredients
         del ingredients
-
     except peewee.DoesNotExist:
-        result = {CATEGORIES_FIELD_NAME: [], INGREDIENTS_FIELD_NAME: [], RECIPES_FIELD_NAME: [],
-                  RECIPE_INGREDIENTS_FIELD_NAME: [],
-                  NEW_UPDATED_FIELD_NAME: int(datetime.timestamp(datetime.now()))}
+        pass
 
     return result
 
@@ -105,15 +134,7 @@ class DeltaHandler(tornado.web.RequestHandler):
         self.write(json.dumps(result))
 
 
-# ======================================== Aliases
-RECIPES_FIELD_NAME = 'recipes'
-CATEGORIES_FIELD_NAME = 'categories'
-INGREDIENTS_FIELD_NAME = 'ingredients'
-RECIPE_INGREDIENTS_FIELD_NAME = 'recipeIngredients'
-NEW_UPDATED_FIELD_NAME = 'newUpdated'
 
-
-# ========================================
 
 
 class UpdateHandler(tornado.web.RequestHandler):
