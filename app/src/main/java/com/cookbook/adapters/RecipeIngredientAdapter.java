5f405a1ca396de7c12356f13b6ee33283476052b
Евгений <Dozer95@bk.ru>
2016-11-23 23:28:54 +0500

    Обновлил иконки и снекбар на экране просмотра рецепта

--- app/src/main/java/com/cookbook/adapters/RecipeIngredientAdapter.java
+++ app/src/main/java/com/cookbook/adapters/RecipeIngredientAdapter.java
@@ -1,8 +1,8 @@
 package com.cookbook.adapters;
 
 import android.content.Context;
-import android.util.Pair;
 import android.support.v7.widget.RecyclerView;
+import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,20 +10,36 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.cookbook.R;
+import com.cookbook.dao.DBShopListHelper;
 import com.cookbook.pojo.Ingredient;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class RecipeIngredientAdapter extends RecyclerView.Adapter<RecipeIngredientAdapter.ViewHolder> {
 
-    private List<Pair<Ingredient,String>> ingredients;
+    private List<Pair<Ingredient, String>> ingredients;
     private Ingredient.IngredientClickListener listener;
-    private Context context;
+    private Set<String> inShopList;
 
-    public RecipeIngredientAdapter(Context context, List<Pair<Ingredient,String>> ingredients, Ingredient.IngredientClickListener listener) {
-        this.context = context;
+    public RecipeIngredientAdapter(Context context, List<Pair<Ingredient, String>> ingredients, Ingredient.IngredientClickListener listener) {
+
         this.ingredients = ingredients;
         this.listener = listener;
+
+        inShopList = new HashSet<>();
+        for (String s : new DBShopListHelper(context).getAll()) {
+            inShopList.add(s.toLowerCase());
+        }
+    }
+
+    public boolean isInList(String ing) {
+        return inShopList.contains(ing.toLowerCase());
+    }
+
+    public void addInList(String ing) {
+        inShopList.add(ing.toLowerCase());
     }
 
     @Override
@@ -41,11 +57,21 @@ public class RecipeIngredientAdapter extends RecyclerView.Adapter<RecipeIngredie
 
         holder.tvCaption.setText(caption);
         holder.tvQuantity.setText(quantity);
+        if (!inShopList.contains(caption.toLowerCase())) {
+            holder.btnAddToShopList.setImageResource(R.drawable.ic_add_to_shop_list);
+        } else {
+            holder.btnAddToShopList.setImageResource(R.drawable.ic_in_shop_list);
+        }
     }
 
     @Override
     public int getItemCount() {
         return ingredients.size();
+    }
+
+    public void removeIng(String ing) {
+        inShopList.remove(ing.toLowerCase());
+        notifyDataSetChanged();
     }
 
     class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
@@ -58,13 +84,14 @@ public class RecipeIngredientAdapter extends RecyclerView.Adapter<RecipeIngredie
 
             tvCaption = (TextView) itemView.findViewById(R.id.tvCaption);
             tvQuantity = (TextView) itemView.findViewById(R.id.tvQuantity);
-            btnAddToShopList = (ImageButton)itemView.findViewById(R.id.btnToShopList);
+            btnAddToShopList = (ImageButton) itemView.findViewById(R.id.btnToShopList);
             btnAddToShopList.setOnClickListener(this);
         }
 
         @Override
         public void onClick(View v) {
             listener.onClick(ingredients.get(getAdapterPosition()).first);
+            notifyItemChanged(getAdapterPosition());
         }
     }
 }
